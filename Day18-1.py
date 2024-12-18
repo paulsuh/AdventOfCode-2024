995b8069bfa0a6b2f28f228839aa5e85acc460a1
Paul Suh <plsuh@goodeast.com>
2024-12-18 00:59:33 -0500

    Day 18 part 1

--- Day18-1.py
+++ Day18-1.py
@@ -1,6 +1,124 @@
 from pprint import pprint
+from collections import deque
+from typing import NamedTuple
+from operator import add
 
-from Inputs.Day18_input import problem_input
+from Inputs.Day18_input import problem_input, map_height, map_width, num_blocks
 
 
-problem_input = """"""
+class Location(NamedTuple):
+    row: int
+    col: int
+
+
+# problem_input = """5,4
+# 4,2
+# 4,5
+# 3,0
+# 2,1
+# 6,3
+# 2,4
+# 1,5
+# 0,6
+# 3,3
+# 2,6
+# 5,1
+# 1,2
+# 5,5
+# 2,5
+# 6,5
+# 1,4
+# 0,4
+# 6,4
+# 1,1
+# 6,1
+# 1,0
+# 0,5
+# 1,6
+# 2,0"""
+#
+# map_height = 7
+# map_width = 7
+# num_blocks = 12
+
+
+blocks_lines = [
+    one_line.split(",")
+    for one_line in problem_input.splitlines()
+]
+
+full_block_list = [
+    (int(y), int(x))
+    for x, y in blocks_lines
+]
+block_list = full_block_list[:num_blocks]
+
+block_list += [
+    (y, map_width)
+    for y in range(map_height)
+]
+block_list += [
+    (y, -1)
+    for y in range(map_height)
+]
+block_list += [
+    (map_height, x)
+    for x in range(map_width)
+]
+block_list += [
+    (-1, x)
+    for x in range(map_width)
+]
+
+pprint(block_list)
+
+
+location_cost: dict[Location, int] = {
+    Location(0, 0): 0
+}
+
+next_location_offsets = (
+    (1, 0),
+    (-1, 0),
+    (0, 1),
+    (0, -1)
+)
+
+
+def get_next_locations(start_loc: Location) -> list[Location]:
+    result = []
+    for one_offset in next_location_offsets:
+        if (next_loc := Location(*map(add, start_loc, one_offset))) not in block_list:
+            result.append(next_loc)
+    return result
+
+
+def get_costs_for_next_locations(start_loc: Location) -> list[Location]:
+    next_locations = get_next_locations(start_loc)
+    result = []
+    current_loc_cost = location_cost.setdefault(start_loc, map_width*map_height + 1)
+    for one_next_loc in next_locations:
+        next_loc_cost = current_loc_cost + 1
+        if next_loc_cost < location_cost.setdefault(one_next_loc, map_width*map_height + 1):
+            location_cost[one_next_loc] = next_loc_cost
+            result.append(one_next_loc)
+
+    return result
+
+
+# x = get_costs_for_next_locations(Location(0, 0))
+# print(x)
+# pprint(location_cost)
+
+
+locations_to_be_checked: deque[Location] = deque()
+locations_to_be_checked.append(Location(0, 0))
+
+while len(locations_to_be_checked) > 0:
+    current_loc = locations_to_be_checked.popleft()
+
+    next_locations = get_costs_for_next_locations(current_loc)
+    locations_to_be_checked.extend(next_locations)
+
+pprint(location_cost)
+print(location_cost[(map_height - 1, map_width - 1)])
